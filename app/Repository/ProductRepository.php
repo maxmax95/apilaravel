f6d05e29382194c5ca02880737309e1c0cdf3a0f
maxmax95 <onagaonaga12@gmail.com>
2024-01-09 20:07:49 -0300

    Melhorias na sintaxe, filtros e conditions funcionando, agora com repository abstrato para outros objetos - v1.0.0

--- app/Repository/ProductRepository.php
+++ app/Repository/ProductRepository.php
@@ -2,38 +2,7 @@
 
 namespace App\Repository;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
-
-class ProductRepository
+class ProductRepository extends AbstractRepository
 {
 
-    private $model;
-
-    public function __construct(Model $model)
-    {
-        $this->model = $model;
-    }
-
-    public function selectConditions($conditions)
-    {
-            $conditions = explode(';', $conditions);
-            foreach($conditions as $condition){
-                $ci = explode(':',$condition);
-                $this->model = $this->model->where($ci[0],$ci[1], $ci[2]);
-
-            }
-    }
-
-    public function selectFilter($filters)
-    {
-       
-            $this->model = $this->model->selectRaw($filters);
-
-    }
-
-    public function getResult()
-    {
-        return $this->model;
-    }
 }
